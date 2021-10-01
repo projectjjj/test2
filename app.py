2dd50fc0557186b94f0454d642caef1e6da8e90b
장민 <>
2021-10-01 23:39:18 +0900

    아쉬운 마음에 재도전!! ㅠㅜ

--- app.py
+++ app.py
@@ -1,39 +1,39 @@
-
 from flask import Flask, render_template, jsonify, request
 from pymongo import MongoClient
 
 app = Flask(__name__)
 
 client = MongoClient("mongodb://localhost:27017/")
-db = client.dbStock
+db = client.dbStock2
 
-import datetime
-
+from datetime import datetime
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
+
 @app.route('/post', methods=['POST'])
 def save_post():
-    title_receive = request.form['title_give']
-    content_receive = request.form['content_give']
-    now = datetime.datetime.now()
-    doc = {'제목': title_receive, '내용': content_receive, '날짜' : now}
-    db.lists.insert_one(doc)
-    return jsonify({'result': '포스팅 성공!'})
+    post_url_receive = request.form['post_url_give']
+    post_comment_receive = request.form['post_comment_give']
+    time = datetime.today().strftime("%Y.%m.%d %H:%M:%S")
+    doc = {'제목': post_url_receive, '내용': post_comment_receive, '날짜' : time}
+    db.users.insert_one(doc)
+    return {"result": "포스팅 성공!"}
+
 
 @app.route('/post', methods=['GET'])
 def get_post():
-    lists = list(db.lists.find({},{'_id':False}))
-    return jsonify({'lists': lists})
-
+    post_list = list(db.users.find({}, {'_id': False}).sort('날짜', -1))
+    return {"result": post_list}
 
 @app.route('/post', methods=['DELETE'])
 def delete_post():
-    return {"result": "success"}
-
+    titie_receive = request.form['delete_give']
+    db.users.delete_one({'제목': titie_receive})
+    return {"result": "삭제하였습니다!"}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
